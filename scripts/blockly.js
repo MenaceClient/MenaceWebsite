20fd0f06489a4042ff9025bdc6e16e8c95ddd192
Exterminate <exterminate5573@gmail.com>
2023-02-03 10:43:59 +1100

    yes

--- scripts/blockly.js
+++ scripts/blockly.js
@@ -5,54 +5,13 @@
     "contents": [
       {
         "kind": "category",
-        "name": "Control",
-        "contents": [
-          {
-            "kind": "block",
-            "type": "controls_if"
-          },
-          {
-            "kind": "block",
-            "type": "controls_repeat_ext"
-          },
-        ]
-      },
-      {
-        "kind": "category",
-        "name": "Logic",
-        "contents": [
-          {
-            "kind": "block",
-            "type": "logic_compare"
-          },
-          {
-            "kind": "block",
-            "type": "logic_operation"
-          },
-          {
-            "kind": "block",
-            "type": "logic_boolean"
-          }
-        ]
-      },
-      {
-        "kind": "category",
         "name": "Variables",
-        "contents": [
-          {
-            "kind": "block",
-            "type": "math_number",
-            "fields": {"NUM": 123}
-          },
-          {
-            "kind": "block",
-            "type": "text"
-          },
-          {
-            "kind": "block",
-            "type": "text_print"
-          },
-        ]
+        "custom": "VARIABLE"
+      },
+      {
+        "kind": "category",
+        "name": "Functions",
+        "custom": "PROCEDURE"
       }
     ]
   }
