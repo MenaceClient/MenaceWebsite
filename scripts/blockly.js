bfd2c80a8af3c38a830ee83982d0a790ffee94ef
Exterminate <exterminate5573@gmail.com>
2023-02-03 10:38:50 +1100

    yes

--- scripts/blockly.js
+++ scripts/blockly.js
@@ -1,37 +1,59 @@
 (function() {
 
   const toolbox = {
-    'kind': 'flyoutToolbox',
+    'kind': 'categoryToolbox',
     "contents": [
       {
-        "kind": "block",
-        "type": "controls_if"
+        "kind": "category",
+        "name": "Control",
+        "contents": [
+          {
+            "kind": "block",
+            "type": "controls_if"
+          },
+          {
+            "kind": "block",
+            "type": "controls_repeat_ext"
+          },
+        ]
       },
       {
-        "kind": "block",
-        "type": "logic_compare"
+        "kind": "category",
+        "name": "Logic",
+        "contents": [
+          {
+            "kind": "block",
+            "type": "logic_compare"
+          },
+          {
+            "kind": "block",
+            "type": "logic_operation"
+          },
+          {
+            "kind": "block",
+            "type": "logic_boolean"
+          }
+        ]
       },
       {
-        "kind": "block",
-        "type": "controls_repeat_ext"
-      },
-      {
-        "kind": "block",
-        "type": "math_number",
-        "fields": {"NUM": 123}
-      },
-      {
-        "kind": "block",
-        "type": "math_arithmetic"
-      },
-      {
-        "kind": "block",
-        "type": "text"
-      },
-      {
-        "kind": "block",
-        "type": "text_print"
-      },
+        "kind": "category",
+        "name": "Variables",
+        "contents": [
+          {
+            "kind": "block",
+            "type": "math_number",
+            "fields": {"NUM": 123}
+          },
+          {
+            "kind": "block",
+            "type": "text"
+          },
+          {
+            "kind": "block",
+            "type": "text_print"
+          },
+        ]
+      }
     ]
   }
 
