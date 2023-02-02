9efe5a8594cc73d77b6f6cc00e651337ef78f946
Exterminate <exterminate5573@gmail.com>
2023-02-03 10:49:21 +1100

    yes

--- scripts/blockly.js
+++ scripts/blockly.js
@@ -49,12 +49,7 @@
           {
             "kind": "block",
             "type": "text"
-          },
-          {
-            "kind": "block",
-            "name": "Variables",
-            "custom": "VARIABLE"
-          },
+          }
         ]
       }
     ]
@@ -64,30 +59,7 @@
     toolbox: toolbox
   });
 
-  /*
-  var blocklyArea = document.getElementById('blocklyArea');
-  var blocklyDiv = document.getElementById('blocklyDiv');
+  //const jsCode = javascriptGenerator.workspaceToCode(workspace);
 
-  var onresize = function(e) {
-    // Compute the absolute coordinates and dimensions of blocklyArea.
-    var element = blocklyArea;
-    var x = 0;
-    var y = 0;
-    do {
-      x += element.offsetLeft;
-      y += element.offsetTop;
-      element = element.offsetParent;
-    } while (element);
-    // Position blocklyDiv over blocklyArea.
-    blocklyDiv.style.left = x + 'px';
-    blocklyDiv.style.top = y + 'px';
-    blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
-    blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
-    Blockly.svgResize(demoWorkspace);
-
-    console.log('resize');
-  };
-  window.addEventListener('resize', onresize, false);
-  onresize();*/
 
 })();
