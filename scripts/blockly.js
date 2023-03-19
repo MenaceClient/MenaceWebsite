7411b2ffaacb8cea164c56db95b16273edef6158
Exterminate <exterminate5573@gmail.com>
2023-03-19 09:11:41 +0000

    bruh

--- scripts/blockly.js
+++ scripts/blockly.js
@@ -72,7 +72,7 @@
         this.setOutput(false);
         this.setNextStatement(false);
         this.setPreviousStatement(false);
-        this.appendField('OnEnable');
+        this.appendStatementInput('OnEnable');
         this.appendStatementInput('DO').appendField('do');
       }
   };
